0c05f658e028f0f1019c70580358e0b246819b6f
HannesNyfelt <hannes.nyfelt@gmail.com>
2024-04-19 13:53:21 +0300

    added en About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+import TopDiv from "@/components/TopDiv";
+
+export default function About() {
+    return (
+        <div>
+            <TopDiv></TopDiv>
+            <div id="about">
+                <div className="text">
+                    <h1>
+                        Wood structure & Johan Keituri
+                    </h1>
+                </div>
+                <div className="text">
+                    <h2>
+                        Ända sedan barnsben har materialet trä fascinerat mig. Trädslagens varierande struktur, färg och hårdhet inspirerar mig i utvecklingen av nya produkter. Jag är utbildad finsnickare och har under de senaste 25 åren arbetat med produktutveckling i olika former.
+                        Träets naturliga struktur och färg gör att varje produkt är unik. Typiskt för företaget är funktionella, mindre produkter där material och form möter varandra på ett spännande sätt.
+                    </h2>
+                </div>
+            </div>
+        </div>
+    )
+
+}
--- src/components/TopDiv.tsx
+++ src/components/TopDiv.tsx
@@ -12,15 +12,13 @@ export default function Example() {
         <Disclosure as="nav" className="bg-white shadow">
             {({ open }) => (
                 <>
-                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div id='TopDiv' className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                         <div className="flex h-16 justify-between">
                             <div className="flex">
                                 <div className="flex flex-shrink-0 items-center">
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                        alt="Your Company"
-                                    />
+                                    <h1>
+                                        W
+                                    </h1>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                                     {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
